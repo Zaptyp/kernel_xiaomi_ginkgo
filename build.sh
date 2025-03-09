21b18cb36ebd6c7e54be8b6c95c82fccd9fb4ef3
Patryk <43276401+Zaptyp@users.noreply.github.com>
2025-03-09 21:03:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ TC_DIR="$HOME/tc/prelude-clang"
 GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
 GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
 AK3_DIR="AnyKernel3"
-DEFCONFIG="ginkgo-stock_defconfig"
+DEFCONFIG="vendor/ginkgo-perf_defconfig"
 export PATH="$TC_DIR/bin:$PATH"
 
 # Build Environment
